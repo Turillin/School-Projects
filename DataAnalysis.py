ac507e47fde560fd56704f408946f8c041993a64
Turillin <zack.rex1@gmail.com>
2025-10-19 17:44:20 +0200

    The raw CSV file that i worked on

--- DataAnalysis.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-import streamlit as st
-import plotly as plt
-
-st.set_page_config(page_title="Analysis of something",
-                   page_icon="🤖",
-                   layout="wide")
